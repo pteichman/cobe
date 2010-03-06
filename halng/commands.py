cd585d328b997eddf3fa23e3da5db9b5c721814a
Peter Teichman <peter@teichman.org>
2010-03-05 22:01:00 -0500

    Add a command for learning from irc logs

--- halng/commands.py
+++ halng/commands.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 import readline
 import sys
 
@@ -62,6 +63,58 @@ class LearnCommand(Command):
         fd = open(filename)
         for line in fd.xreadlines():
             b.learn(line.strip())
+
+class LearnIrcLogCommand(Command):
+    def __init__(self):
+        Command.__init__(self, "learn-irc-log", summary="Learn an irc log")
+
+    def run(self, options, args):
+        if len(args) != 1:
+            log.error("usage: learn-irc-log <irc log file>")
+            return
+
+        filename = args[0]
+
+        b = Brain("hal.brain")
+
+        s = os.stat(filename)
+        size_left = s.st_size
+
+        count = 0
+
+        fd = open(filename)
+        for line in fd.xreadlines():
+            size_left = size_left - len(line)
+
+            count = count + 1
+            if (count % 100) == 0:
+                complete = 100 * (1. - float(size_left) / float(s.st_size))
+                sys.stdout.write("\r%.0f%%" % complete)
+                sys.stdout.flush()
+
+            msg = self._parse_irc_message(line.strip())
+            if msg:
+                b.learn(msg)
+
+        sys.stdout.write("\r100%\n")
+        sys.stdout.flush()
+
+    def _parse_irc_message(self, msg):
+        # only match lines of the form "HH:MM <nick> message"
+        match = re.match("\d+:\d+\s+<\S+>\s+(.*)", msg)
+        if not match:
+            return None
+
+        msg = match.group(1)
+
+        # strip "username: " at the beginning of messages
+        msg = re.sub("^\S+[,:]\s+", "", msg)
+
+        # strip kibot style '"asdf" --user, 06-oct-09' quotes
+        msg = re.sub("\"(.*)\" --\S+,\s+\d+-\S+-\d+",
+                     lambda m: m.group(1), msg)
+
+        return msg
 
 class ConsoleCommand(Command):
     def __init__(self):
