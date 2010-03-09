70138da1fa6a28d0e7b7fdf80ab894236c0f5583
Peter Teichman <peter@teichman.org>
2010-03-08 20:52:53 -0500

    Make all the tokenizer unit tests assertEquals() on arrays

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -11,35 +11,19 @@ class testMegaHALTokenizer(unittest.TestCase):
 
     def testSplitSentence(self):
         words = self.tokenizer.split("hi.")
-        self.assertEquals(len(words), 2)
-        self.assertEquals(words[0], "HI")
-        self.assertEquals(words[1], ".")
+        self.assertEquals(words, ["HI", "."])
 
     def testSplitComma(self):
         words = self.tokenizer.split("hi, cobe")
-        self.assertEquals(len(words), 4)
-        self.assertEquals(words[0], "HI")
-        self.assertEquals(words[1], ", ")
-        self.assertEquals(words[2], "COBE")
-        self.assertEquals(words[3], ".")
+        self.assertEquals(words, ["HI", ", ", "COBE", "."])
 
     def testSplitImplicitStop(self):
         words = self.tokenizer.split("hi")
-        self.assertEquals(len(words), 2)
-        self.assertEquals(words[0], "HI")
-        self.assertEquals(words[1], ".")
+        self.assertEquals(words, ["HI", "."])
 
     def testSplitUrl(self):
         words = self.tokenizer.split("http://www.google.com/")
-        self.assertEquals(len(words), 8)
-        self.assertEquals(words[0], "HTTP")
-        self.assertEquals(words[1], "://")
-        self.assertEquals(words[2], "WWW")
-        self.assertEquals(words[3], ".")
-        self.assertEquals(words[4], "GOOGLE")
-        self.assertEquals(words[5], ".")
-        self.assertEquals(words[6], "COM")
-        self.assertEquals(words[7], "/.")
+        self.assertEquals(words, ["HTTP", "://", "WWW", ".", "GOOGLE", ".", "COM", "/."])
 
 if __name__ == '__main__':
     unittest.main()
