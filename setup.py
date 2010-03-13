e236ae65168ad47739dda5f14a66462de25867d8
Peter Teichman <peter@teichman.org>
2010-03-13 08:48:17 -0500

    Clean up setup.py metadata for release to the Python Package Index

--- setup.py
+++ setup.py
@@ -8,7 +8,17 @@ setup(
     name = "cobe",
     version = "0.5",
     author = "Peter Teichman",
-    license = "MIT",
-    packages = find_packages(exclude=["tests"]),
+    author_email = "peter@teichman.org",
+    packages = ["cobe"],
     test_suite = "tests.cobe_suite",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ]
 )
