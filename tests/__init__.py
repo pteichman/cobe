90d7c3da84bad4b39bc0d76849aadb6e2e8c7a6c
Peter Teichman <peter@teichman.org>
2010-03-14 21:59:26 -0400

    Rename test_cobe to test_brain

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,13 @@
 import unittest
 
 # import our test modules
+import test_brain
 import test_commands
-import test_cobe
 import test_tokenizers
 
 __all__ = ["cobe_suite"]
 
 cobe_suite = unittest.TestSuite()
 cobe_suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_commands))
-cobe_suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_cobe))
+cobe_suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_brain))
 cobe_suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_tokenizers))
