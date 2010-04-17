3d2872bb64a12253b372b6473ae9896410f57a81
Peter Teichman <peter@teichman.org>
2010-04-17 18:51:25 -0400

    Update version to 1.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "cobe",
-    version = "1.0.1",
+    version = "1.0.2",
     author = "Peter Teichman",
     author_email = "peter@teichman.org",
     url = "http://wiki.github.com/pteichman/cobe/",
