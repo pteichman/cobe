c07d314e44f05554cf55a5b3fa9703f44adeece7
Peter Teichman <peter@teichman.org>
2010-04-27 22:09:18 -0400

    Initial import of instatrace

---
+++ cobe/instatrace.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2010 Peter Teichman
+
+import math
+import time
+
+def singleton(cls):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
+
+@singleton
+class Instatrace:
+    def __init__(self):
+        self._fd = None
+
+    def init(self, filename):
+        self._fd = open(filename, "w+")
+
+    def is_enabled(self):
+        return self._fd is not None
+
+    def now(self):
+        """High resolution, integer now"""
+        if not self.is_enabled():
+            return 0
+        return int(time.time()*100000)
+
+    def trace(self, statName, statValue, userData=None):
+        if not self.is_enabled():
+            return
+        extra = ""
+        if userData is not None:
+            extra = " " + repr(userData)
+
+        self._fd.write("%s %d%s\n" % (statName, statValue, extra))
