a0205bd68ec84834821752f0128eabd751194785
Peter Teichman <peter@teichman.org>
2012-01-21 11:25:39 -0500

    Prewarm the kernel buffer cache on start
    This is a major performance improvement on a large brain, bringing
    me from ~10 replies in half a second to well over 200 on average.
    
    Prewarming the cache helps a great deal because of a combination
    of factors. Brain data that is tightly connected is unlikely to
    have good locality in the database, and the random walk for
    candidate reply generation means that any database page could be
    accessed during a reply.
    
    Generating more candidate replies would help warm the cache, but
    since that's a timed operation, a cold cache means fewer replies
    and fewer pages get cached.

--- cobe/brain.py
+++ cobe/brain.py
@@ -39,6 +39,9 @@ class Brain:
             log.info("File does not exist. Assuming defaults.")
             Brain.init(filename)
 
+        self.filename = filename
+        self.prewarm_caches()
+
         with trace_us("Brain.connect_us"):
             self.graph = graph = Graph(sqlite3.connect(filename))
 
@@ -75,6 +78,15 @@ class Brain:
         self._end_context_id = graph.get_node_by_tokens(self._end_context)
 
         self._learning = False
+
+    def prewarm_caches(self):
+        # Warm the kernel buffer cache. This is heavy-handed, but a
+        # major performance improvement on a large brain. Since reply
+        # quality is tied tightly to how many candiates can be
+        # generated, this improves quality as well.
+        with open(self.filename, "r") as fd:
+            with trace_us("Brain.cache_prewarm_us"):
+                fd.read()
 
     def start_batch_learning(self):
         """Begin a series of batch learn operations. Data will not be
