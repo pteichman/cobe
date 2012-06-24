094f029ebfe6653a8e83d8d467f6962bfd2d53be
Peter Teichman <peter@teichman.org>
2012-06-24 10:13:43 -0400

    Add encoders/decoders for varint variable-width integer encoding
    This is preparation for a new on-disk language model, which will
    compress its n-grams and their counts with varints.
    
    This implementation hasn't had any work for performance, and
    it's likely it can be made faster.

---
+++ cobe/varint.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2012 Peter Teichman
+
+import array
+
+# A few routines for encoding & decoding varint-encoded integers, as
+# described in the Protocol Buffers docs:
+# https://developers.google.com/protocol-buffers/docs/encoding#varints
+#
+# varint is a 7-bit integer encoding with the 8th reserved as a
+# continuation bit. This is an implementation of the unsigned encoding
+# only (not ZigZag).
+
+def encode_one(value):
+    return encode((value,))
+
+def encode(values):
+    buf = array.array("B")
+    append = buf.append
+
+    for value in values:
+        bits = value & 0x7f
+        value >>= 7
+        while value:
+            append(0x80 | bits)
+            bits = value & 0x7f
+            value >>= 7
+        append(bits)
+
+    return buf.tostring()
+
+def decode_one(value):
+    return decode(value)[0]
+
+def decode(data):
+    ret = []
+    append = ret.append
+    local_ord = ord
+
+    cur = pos = shift = 0
+    end = len(data)
+
+    while pos < end:
+        b = local_ord(data[pos])
+        cur |= ((b & 0x7f) << shift)
+
+        # If the high bit is set, continue reading the current int
+        if b & 0x80:
+            shift += 7
+        else:
+            append(cur)
+            shift = 0
+            cur = 0
+
+        pos += 1
+
+    return ret
---
+++ tests/test_varint.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2012 Peter Teichman
+
+import unittest
+
+from cobe.varint import decode, decode_one, encode, encode_one
+
+
+class TestVarint(unittest.TestCase):
+    def test_varint_size(self):
+        # Check each boundary of varint sizing
+        sizes = {
+            2: 0x80,
+            3: 0x4000,
+            4: 0x200000,
+            5: 0x10000000,
+            6: 0x800000000L,
+            7: 0x40000000000L,
+            8: 0x2000000000000L,
+            9: 0x100000000000000L,
+            10: 0x8000000000000000L
+            }
+
+        for i in xrange(128):
+            self.assertEquals(1, len(encode_one(i)))
+
+        for size, value in sizes.items():
+            self.assertEquals(size - 1, len(encode_one(value - 1)))
+            self.assertEquals(size, len(encode_one(value)))
+
+    def test_encode_one(self):
+        # numbers 0..127 get encoded as a single byte
+        for i in xrange(128):
+            self.assertEquals(chr(i), encode_one(i))
+
+    def test_decode_one(self):
+        for i in xrange(64, 128):
+            self.assertEquals(i, decode_one(chr(i)))
+
+    def test_encode_decode_one(self):
+        for i in xrange(100000):
+            self.assertEquals(i, decode_one(encode_one(i)))
+
+    def test_encode_decode(self):
+        nums = range(0, 2048)
+
+        data = encode(nums)
+        self.assertEquals(3968, len(data))
+
+        nums2 = decode(data)
+        self.assertEquals(nums, nums2)
+
+if __name__ == '__main__':
+    unittest.main()
