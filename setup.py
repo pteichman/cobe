7cd3fb3c4edb329684adb0c54d897d9e2f1e1c83
Peter Teichman <peter@teichman.org>
2012-09-08 21:41:16 -0400

    Unify all tests running under nosetests
    This means:
    
    * command line tests during development
    * the Travis CI configuration for github-integrated testing
    * tox for local dist builds

--- setup.py
+++ setup.py
@@ -16,9 +16,19 @@ setup(
     url = "http://wiki.github.com/pteichman/cobe/",
     description = "Markov chain based text generator library and chatbot",
     packages = ["cobe"],
+
+    # setup_require nosetests, so that the nosetests command is
+    # available immediately in a fresh checkout
+    setup_requires = [
+        "nose==1.1.2"
+    ],
+
     test_suite = "unittest2.collector",
 
+    # mock and unittest2 are required by the tests; coverage is
+    # required by the [nosetests] section of setup.cfg.
     tests_require = [
+        "coverage==3.5.2",
         "mock==1.0b1",
         "unittest2==0.5.1"
     ],
