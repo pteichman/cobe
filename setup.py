23f015a7a5e1fccfad4437c6ee11be2fee62a260
Peter Teichman <peter@teichman.org>
2014-10-20 07:33:44 -0700

    Bump version to 2.1.1; roll a release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cobe",
-    version = "2.1.0",
+    version = "2.1.1",
     author = "Peter Teichman",
     author_email = "peter@teichman.org",
     url = "http://wiki.github.com/pteichman/cobe/",
