60b73763fa872565496d2eda5dd4ffa64d83b7f5
Gustavo6046 <rehermann6046@gmail.com>
2021-07-08 17:10:45 -0300

    Don't use encode() in privmsg call
    The irc library is already fit for usage in Python 3. As such, using 
    encode() is unnecessary.

--- cobe/bot.py
+++ cobe/bot.py
@@ -98,4 +98,4 @@ class IrcLogHandler(logging.Handler):
         conn = self.connection
 
         if conn.is_connected():
-            conn.privmsg(self.channel, record.getMessage().encode("utf-8"))
+            conn.privmsg(self.channel, record.getMessage())
